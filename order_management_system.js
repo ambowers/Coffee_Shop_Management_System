879ec6aad8fd35da35ccb37c66538a6e61e4f6b1
lina <ambowers@usf.edu>
2024-09-18 18:21:32 -0400

    Initialize order array

--- order_management_system.js
+++ order_management_system.js
@@ -6,4 +6,9 @@ const inventory = [
     { name: 'Cappuccino', price: 4, quantity: 6 }, 
     { name: 'Mocha', price: 5, quantity: 4 } 
  ];
- 
+
+ // Task 2  create an orders array of order objects
+
+let orders =[]
+    // i left the array empty to store the customer orders
+    
